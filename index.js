a532a182964cef6bf311144fdb03639c980b12d4
Morine Achieng <morineachieng@Morines-MacBook-Air.local>
2023-06-16 04:21:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetLength){
+let result
+if(feetLength<=400){
+  result='This one is on me!'
+}
+else if(feetLength>2000){
+  result='I will gladly take your thirty bucks.'
+}
+if (feetLength>2500){
+  result='No can do.'
+}
+return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  let message;
+  if(city='NYC'){
+    message='Ok, sounds good.'
+  }
+  else(city='Pittsburg'){
+    message='No go.'
+  }
+  return result
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function switchOnCharmFromTip(){}
